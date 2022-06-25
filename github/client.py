c6c37a3a7602e3a331cc723b29afc2fb669a9583
Vagner Kuntz <vaagner.kuntz@gmail.com>
2022-06-24 22:58:30 -0300

    chore: add [S]ingle Responsibility Principle

---
+++ github/client.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+class GithubClient():
+
+    API_BASE_URL = "https://api.github.com"
+
+    @classmethod
+    def get_repos_by_user(self, user):
+        response = requests.get(
+            f'{self.API_BASE_URL}/users/{user}/repos')
+        if response.status_code == 200:
+            return {"status_code": 200, "body": response.json()}
+        else:
+            return {"status_code": response.status_code, "body": "Error while getting repositories"}
